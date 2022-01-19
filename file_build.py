b5b874f0164746c5d032d7b22d091029f47f0f85
Ebraham <73792019+drskaf@users.noreply.github.com>
2022-01-19 09:28:09 +0000

    Update file_build.py

--- file_build.py
+++ file_build.py
@@ -1,12 +1,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import os
 import numpy as np
-import tensorflow as tf
-from tensorflow.keras import layers
-import tensorflow_probability as tfp
-import matplotlib.pyplot as plt
 import pandas as pd
-import aequitas as ae
 import os
 from itertools import chain
 
