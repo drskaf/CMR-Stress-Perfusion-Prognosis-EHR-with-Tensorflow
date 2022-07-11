4d774f188a8ca0848bc77ac81d14bf2481be4074
Ebraham <73792019+drskaf@users.noreply.github.com>
2022-07-11 13:26:47 +0100

    Update clinical_model_compare.py

--- clinical_model_compare.py
+++ clinical_model_compare.py
@@ -1,16 +1,20 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import os
 import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow_probability as tfp
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
-from sklearn.metrics import roc_auc_score, accuracy_score, f1_score, roc_curve
+from sklearn.metrics import roc_auc_score, accuracy_score, f1_score, roc_curve, classification_report
 import matplotlib.pyplot as plt
 import pandas as pd
 from utils import  patient_dataset_splitter, build_vocab_files, show_group_stats_viz, aggregate_dataset, preprocess_df, df_to_dataset, posterior_mean_field, prior_trainable
 from plot_metric.functions import BinaryClassification
 import pickle
+from keras.models import model_from_json, load_model
 
 pd.set_option('display.max_columns', 500)
 
@@ -18,14 +22,14 @@ pd.set_option('display.max_columns', 500)
 survival_df = pd.read_csv('final.csv')
 survival_df['Gender'] = survival_df['patient_GenderCode'].astype('category')
 survival_df['Gender'] = survival_df['Gender'].cat.codes
-survival_df['Chronic_kidney_disease'] = survival_df['Chronic_kidney_disease_(disorder)'].astype(str)
+survival_df['Chronic_kidney_disease'] = survival_df['Chronic_kidney_disease_(disorder)']
 survival_df['Age'] = survival_df['Age_on_20.08.2021'].astype(int)
-survival_df['Hypertension'] = survival_df['Essential_hypertension'].astype(str)
-survival_df['Gender'] = survival_df['Gender'].astype(str)
-survival_df['Heart_failure'] = survival_df['Heart_failure_(disorder)'].astype(str)
+survival_df['Hypertension'] = survival_df['Essential_hypertension']
+survival_df['Gender'] = survival_df['Gender']
+survival_df['Heart_failure'] = survival_df['Heart_failure_(disorder)']
 
 # Define columns
-categorical_col_list = ['Chronic_kidney_disease','Hypertension', 'Gender', 'Heart_failure' ]
+categorical_col_list = ['Positive_perf', 'Positive_LGE', 'Chronic_kidney_disease','Hypertension', 'Gender', 'Heart_failure' ]
 numerical_col_list= ['Age']
 PREDICTOR_FIELD = 'Event'
 
@@ -36,16 +40,11 @@ def select_model_features(df, categorical_col_list, numerical_col_list, PREDICTO
 selected_features_df = select_model_features(survival_df, categorical_col_list, numerical_col_list,
                                              PREDICTOR_FIELD)
 
-for c in categorical_col_list:
-    selected_features_df[c] = selected_features_df[c].astype(bool)
-
 # Split data
 d_train, d_val, d_test = patient_dataset_splitter(selected_features_df, 'patient_TrustNumber')
 d_train = d_train.drop(columns=['patient_TrustNumber'])
 d_val = d_val.drop(columns=['patient_TrustNumber'])
-d_train.to_csv('train_data.csv')
-d_val.to_csv('valid_data.csv')
-d_test.to_csv('test_data.csv')
+d_test = d_test.drop(columns=['patient_TrustNumber'])
 
 x_train = d_train[categorical_col_list + numerical_col_list]
 y_train = d_train[PREDICTOR_FIELD]
@@ -82,8 +81,66 @@ print('RF ROCAUC score:',roc_auc_score(y_test, rfc_predict))
 print('RF Accuracy score:',accuracy_score(y_test, rfc_predict))
 print('RF F1 score:',f1_score(y_test, rfc_predict))
 
+# test neural network model
+
+# Load and preprocess test data
+test_data = pd.read_csv('test_data.csv')
+
+processed_df = preprocess_df(test_data, categorical_col_list,
+        numerical_col_list, PREDICTOR_FIELD, categorical_impute_value='nan', numerical_impute_value=0)
+
+# Convert dataset from Pandas dataframes to TF dataset
+batch_size = 1
+survival_test_ds = df_to_dataset(processed_df, PREDICTOR_FIELD, batch_size=batch_size)
+
+# Create categorical features
+vocab_file_list = build_vocab_files(test_data, categorical_col_list)
+from student_utils import create_tf_categorical_feature_cols
+tf_cat_col_list = create_tf_categorical_feature_cols(categorical_col_list)
+
+# create numerical features
+def create_tf_numerical_feature_cols(numerical_col_list, test_df):
+    tf_numeric_col_list = []
+    for c in numerical_col_list:
+        tf_numeric_feature = tf.feature_column.numeric_column(c)
+        tf_numeric_col_list.append(tf_numeric_feature)
+    return tf_numeric_col_list
+
+tf_cont_col_list = create_tf_numerical_feature_cols(numerical_col_list, test_data)
+
+# Create feature layer
+claim_feature_columns = tf_cat_col_list + tf_cont_col_list
+claim_feature_layer = tf.keras.layers.DenseFeatures(claim_feature_columns)
+
+with open('fcn_b.pkl', 'rb') as pickle_file:
+    content = pickle.load(pickle_file)
+survival_model = pickle.load(open('fcn_b.pkl', 'rb'))
+
+# Predict with model
+preds = survival_model.predict(survival_test_ds)
+pred_test = []
+for p in preds:
+    pred = np.argmax(p)
+    pred_test.append(pred)
+print(pred_test[:5])
+survival_yhat = list(test_data['Event'].values)
+print(survival_yhat[:5])
+
+prob_outputs = {
+    "pred": pred_test,
+    "actual_value": survival_yhat
+}
+prob_output_df = pd.DataFrame(prob_outputs)
+print(prob_output_df.head())
+
+# Evaluate model
+print(classification_report(survival_yhat, pred_test))
+print('FCN ROCAUC score:',roc_auc_score(survival_yhat, pred_test))
+print('FCN Accuracy score:',accuracy_score(survival_yhat, pred_test))
+print('FCN F1 score:',f1_score(survival_yhat, pred_test))
+
 # build XGBoost Classifier model
-xgb_model = XGBClassifier().fit(x_train, y_train)
+xgb_model = XGBClassifier(tree_method="hist",enable_categorical=True).fit(x_train, y_train)
 
 xgb_predict = xgb_model.predict(x_test)
 print('XGB ROCAUC score:',roc_auc_score(y_test, xgb_predict))
@@ -103,9 +160,11 @@ plt.plot(fpr, tpr, label="Random Forest Model, AUC="+str(auc))
 fpr, tpr, _ = roc_curve(y_test, xgb_predict)
 auc = round(roc_auc_score(y_test, xgb_predict), 2)
 plt.plot(fpr, tpr, label="XGBoost Classifier Model, AUC="+str(auc))
+fpr, tpr, _ = roc_curve(survival_yhat, pred_test)
+auc = round(roc_auc_score(survival_yhat, pred_test), 2)
+plt.plot(fpr, tpr, label="Neural Network Model, AUC="+str(auc))
 plt.legend()
 plt.show()
-
 
 
 
