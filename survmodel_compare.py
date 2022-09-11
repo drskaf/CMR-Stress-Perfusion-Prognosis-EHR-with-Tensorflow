eb07d8ff6ba81f7579474e7468c56c3af797f931
Ebraham Alskaf <73792019+drskaf@users.noreply.github.com>
2022-09-11 09:55:58 +0100

    Update and rename clinical_survmodel_compare.py to survmodel_compare.py

--- survmodel_compare.py
+++ survmodel_compare.py
@@ -6,15 +6,17 @@ from tensorflow import keras
 import tensorflow_probability as tfp
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 from xgboost import XGBClassifier
 from sklearn.metrics import roc_auc_score, accuracy_score, f1_score, roc_curve, classification_report
 import matplotlib.pyplot as plt
 import pandas as pd
-from utils import  patient_dataset_splitter, build_vocab_files, show_group_stats_viz, aggregate_dataset, preprocess_df, df_to_dataset, posterior_mean_field, prior_trainable
+from student_utils import patient_dataset_splitter
+from utils import build_vocab_files, show_group_stats_viz, aggregate_dataset, preprocess_df, df_to_dataset, posterior_mean_field, prior_trainable
 from plot_metric.functions import BinaryClassification
 import pickle
 from keras.models import model_from_json, load_model
+import functools
 
 pd.set_option('display.max_columns', 500)
 
@@ -27,11 +29,23 @@ survival_df['Age'] = survival_df['Age_on_20.08.2021'].astype(int)
 survival_df['Hypertension'] = survival_df['Essential_hypertension']
 survival_df['Gender'] = survival_df['Gender']
 survival_df['Heart_failure'] = survival_df['Heart_failure_(disorder)']
+survival_df['Smoking'] = survival_df['Smoking_history'].astype(int)
+survival_df['LVEF'] = survival_df['LVEF_(%)']
 
 # Define columns
-categorical_col_list = ['Positive_perf', 'Positive_LGE', 'Chronic_kidney_disease','Hypertension', 'Gender', 'Heart_failure' ]
-numerical_col_list= ['Age']
+categorical_col_list = ['Chronic_kidney_disease','Hypertension', 'Gender', 'Heart_failure', 'Smoking', 'Positive_LGE', 'Positive_perf']
+numerical_col_list= ['Age', 'LVEF']
 PREDICTOR_FIELD = 'Event'
+
+for v in survival_df['Age'].values:
+    mean = survival_df['Age'].describe()['mean']
+    std = survival_df['Age'].describe()['std']
+    v = v - mean / std
+
+for x in survival_df['LVEF'].values:
+    mean = survival_df['LVEF'].describe()['mean']
+    std = survival_df['LVEF'].describe()['std']
+    x = x - mean / std
 
 def select_model_features(df, categorical_col_list, numerical_col_list, PREDICTOR_FIELD, grouping_key='patient_TrustNumber'):
     selected_col_list = [grouping_key] + [PREDICTOR_FIELD] + categorical_col_list + numerical_col_list
@@ -48,8 +62,8 @@ d_test = d_test.drop(columns=['patient_TrustNumber'])
 
 x_train = d_train[categorical_col_list + numerical_col_list]
 y_train = d_train[PREDICTOR_FIELD]
-x_test = d_test[categorical_col_list + numerical_col_list]
-y_test = d_test[PREDICTOR_FIELD]
+x_test = d_val[categorical_col_list + numerical_col_list]
+y_test = d_val[PREDICTOR_FIELD]
 
 # fit SVM model
 svc_model = SVC(class_weight='balanced', probability=True)
@@ -84,10 +98,20 @@ print('RF F1 score:',f1_score(y_test, rfc_predict))
 # test neural network model
 
 # Load and preprocess test data
-test_data = pd.read_csv('test_data.csv')
+test_data = d_val
 
 processed_df = preprocess_df(test_data, categorical_col_list,
         numerical_col_list, PREDICTOR_FIELD, categorical_impute_value='nan', numerical_impute_value=0)
+
+for v in processed_df['Age'].values:
+    mean = processed_df['Age'].describe()['mean']
+    std = processed_df['Age'].describe()['std']
+    v = v - mean / std
+
+for x in processed_df['LVEF'].values:
+    mean = processed_df['LVEF'].describe()['mean']
+    std = processed_df['LVEF'].describe()['std']
+    x = x - mean / std
 
 # Convert dataset from Pandas dataframes to TF dataset
 batch_size = 1
@@ -112,32 +136,32 @@ tf_cont_col_list = create_tf_numerical_feature_cols(numerical_col_list, test_dat
 claim_feature_columns = tf_cat_col_list + tf_cont_col_list
 claim_feature_layer = tf.keras.layers.DenseFeatures(claim_feature_columns)
 
-with open('fcn_b.pkl', 'rb') as pickle_file:
+with open('fcn1.pkl', 'rb') as pickle_file:
     content = pickle.load(pickle_file)
-survival_model = pickle.load(open('fcn_b.pkl', 'rb'))
+survival_model = pickle.load(open('fcn1.pkl', 'rb'))
 
 # Predict with model
 preds = survival_model.predict(survival_test_ds)
-pred_test = []
+pred_test_cl = []
 for p in preds:
     pred = np.argmax(p)
-    pred_test.append(pred)
-print(pred_test[:5])
+    pred_test_cl.append(pred)
+print(pred_test_cl[:5])
 survival_yhat = list(test_data['Event'].values)
 print(survival_yhat[:5])
 
 prob_outputs = {
-    "pred": pred_test,
+    "pred": pred_test_cl,
     "actual_value": survival_yhat
 }
 prob_output_df = pd.DataFrame(prob_outputs)
 print(prob_output_df.head())
 
 # Evaluate model
-print(classification_report(survival_yhat, pred_test))
-print('FCN ROCAUC score:',roc_auc_score(survival_yhat, pred_test))
-print('FCN Accuracy score:',accuracy_score(survival_yhat, pred_test))
-print('FCN F1 score:',f1_score(survival_yhat, pred_test))
+print(classification_report(survival_yhat, pred_test_cl))
+print('Clinical FCN ROCAUC score:',roc_auc_score(survival_yhat, pred_test_cl))
+print('Clinical FCN Accuracy score:',accuracy_score(survival_yhat, pred_test_cl))
+print('Clinical FCN F1 score:',f1_score(survival_yhat, pred_test_cl))
 
 # build XGBoost Classifier model
 xgb_model = XGBClassifier(tree_method="hist",enable_categorical=True).fit(x_train, y_train)
@@ -147,24 +171,38 @@ print('XGB ROCAUC score:',roc_auc_score(y_test, xgb_predict))
 print('XGB Accuracy score:',accuracy_score(y_test, xgb_predict))
 print('XGB F1 score:',f1_score(y_test, xgb_predict))
 
+# build ensemble method
+comb_model = VotingClassifier(estimators=[('XBG',xgb_model), ('LR',lr), ('RF',rfc), ('SVC',svc_model)], voting='hard')
+comb_model.fit(x_train, y_train)
+comb_model_pred = comb_model.predict(x_test)
+
+print('Ensemble Model ROCAUC score:',roc_auc_score(y_test, comb_model_pred))
+print('Ensemble Model Accuracy score:',accuracy_score(y_test, comb_model_pred))
+print('Ensemble Model F1 score:',f1_score(y_test, comb_model_pred))
+
 # plot AUC
 fpr, tpr, _ = roc_curve(y_test, svc_predict)
 auc = round(roc_auc_score(y_test, svc_predict), 2)
-plt.plot(fpr,tpr,label="SVM Model, AUC="+str(auc))
+plt.plot(fpr,tpr,label="SVM, AUC="+str(auc))
 fpr, tpr, _ = roc_curve(y_test, lr_predict)
 auc = round(roc_auc_score(y_test, lr_predict), 2)
-plt.plot(fpr, tpr, label="Linear Regression Model, AUC="+str(auc))
+plt.plot(fpr, tpr, label="Multivariate Regression, AUC="+str(auc))
 fpr, tpr, _ = roc_curve(y_test, rfc_predict)
 auc = round(roc_auc_score(y_test, rfc_predict), 2)
-plt.plot(fpr, tpr, label="Random Forest Model, AUC="+str(auc))
+plt.plot(fpr, tpr, label="Random Forest, AUC="+str(auc))
 fpr, tpr, _ = roc_curve(y_test, xgb_predict)
 auc = round(roc_auc_score(y_test, xgb_predict), 2)
-plt.plot(fpr, tpr, label="XGBoost Classifier Model, AUC="+str(auc))
-fpr, tpr, _ = roc_curve(survival_yhat, pred_test)
-auc = round(roc_auc_score(survival_yhat, pred_test), 2)
-plt.plot(fpr, tpr, label="Neural Network Model, AUC="+str(auc))
+plt.plot(fpr, tpr, label="XGBoost Classifier, AUC="+str(auc))
+fpr, tpr, _ = roc_curve(survival_yhat, pred_test_cl)
+auc = round(roc_auc_score(survival_yhat, pred_test_cl), 2)
+plt.plot(fpr, tpr, label="Fully Connected Network, AUC="+str(auc))
+fpr, tpr, _ = roc_curve(y_test, comb_model_pred)
+auc = round(roc_auc_score(y_test, comb_model_pred), 2)
+plt.plot(fpr, tpr, label="Ensemble Classifier, AUC="+str(auc))
 plt.legend()
+plt.xlabel('1 - Specificity')
+plt.ylabel('Sensitivity')
+plt.title('Survival Models Comparison')
 plt.show()
-
 
 
