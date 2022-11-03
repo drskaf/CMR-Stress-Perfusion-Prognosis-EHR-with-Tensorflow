cad5ec60fcae496199223c4671838f1336320074
Ebraham Alskaf <73792019+drskaf@users.noreply.github.com>
2022-11-03 20:49:13 +0000

    Update survmodel_compare.py

--- survmodel_compare.py
+++ survmodel_compare.py
@@ -3,19 +3,22 @@ import os
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-import tensorflow_probability as tfp  
+import tensorflow_probability as tfp
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 from xgboost import XGBClassifier
 from sklearn.metrics import roc_auc_score, accuracy_score, f1_score, roc_curve, classification_report
 import matplotlib.pyplot as plt
 import pandas as pd
-from utils import patient_dataset_splitter_compare, build_vocab_files, show_group_stats_viz, aggregate_dataset, preprocess_df, df_to_dataset, posterior_mean_field, prior_trainable
+from student_utils import patient_dataset_bi_splitter
+from utils import patient_dataset_splitter, build_vocab_files, show_group_stats_viz, aggregate_dataset, preprocess_df, df_to_dataset, posterior_mean_field, prior_trainable
 from plot_metric.functions import BinaryClassification
 import pickle
 from keras.models import model_from_json, load_model
-import functools
+import scipy.stats
+from sklearn.preprocessing import LabelBinarizer
 
 pd.set_option('display.max_columns', 500)
 
@@ -54,12 +57,15 @@ selected_features_df = select_model_features(survival_df, categorical_col_list, 
                                              PREDICTOR_FIELD)
 
 # Split data
-d_train, d_test = patient_dataset_splitter_compare(selected_features_df, 'patient_TrustNumber')
+d_train, d_val, d_test = patient_dataset_splitter(selected_features_df, 'patient_TrustNumber')
 d_train = d_train.drop(columns=['patient_TrustNumber'])
+d_val = d_val.drop(columns=['patient_TrustNumber'])
 d_test = d_test.drop(columns=['patient_TrustNumber'])
 
 x_train = d_train[categorical_col_list + numerical_col_list]
 y_train = d_train[PREDICTOR_FIELD]
+x_val = d_val[categorical_col_list + numerical_col_list]
+y_val = d_val[PREDICTOR_FIELD]
 x_test = d_test[categorical_col_list + numerical_col_list]
 y_test = d_test[PREDICTOR_FIELD]
 
@@ -68,6 +74,7 @@ svc_model = SVC(class_weight='balanced', probability=True)
 
 svc_model.fit(x_train, y_train)
 svc_predict = svc_model.predict(x_test)
+svc_preds = svc_model.predict_proba(x_test)[:,1]
 
 print('SVM ROCAUC score:',roc_auc_score(y_test, svc_predict))
 print('SVM Accuracy score:',accuracy_score(y_test, svc_predict))
@@ -75,20 +82,30 @@ print('SVM F1 score:',f1_score(y_test, svc_predict))
 
 # build linear regression model
 lr = LogisticRegression()
-
 lr.fit(x_train, y_train)
 
 lr_predict = lr.predict(x_test)
+lr_preds = lr.predict_proba(x_test)[:,1]
+
 print('LR ROCAUC score:',roc_auc_score(y_test, lr_predict))
 print('LR Accuracy score:',accuracy_score(y_test, lr_predict))
 print('LR F1 score:',f1_score(y_test, lr_predict))
+
+fpr, tpr, _ = roc_curve(y_test, lr_preds)
+auc = round(roc_auc_score(y_test, lr_preds), 2)
+plt.plot(fpr, tpr, label="Linear Model, AUC="+str(auc))
+plt.legend()
+plt.xlabel('1 - Specificity')
+plt.ylabel('Sensitivity')
+plt.show()
 
 # build random forest model
 rfc = RandomForestClassifier()
 
 rfc.fit(x_train, y_train)
+rfc_predict = rfc.predict(x_test)
+rfc_preds = rfc.predict_proba(x_test)[:,1]
 
-rfc_predict = rfc.predict(x_test)# check performance
 print('RF ROCAUC score:',roc_auc_score(y_test, rfc_predict))
 print('RF Accuracy score:',accuracy_score(y_test, rfc_predict))
 print('RF F1 score:',f1_score(y_test, rfc_predict))
@@ -165,43 +182,55 @@ print('Clinical FCN F1 score:',f1_score(survival_yhat, pred_test_cl))
 xgb_model = XGBClassifier(tree_method="hist",enable_categorical=True).fit(x_train, y_train)
 
 xgb_predict = xgb_model.predict(x_test)
+xgb_preds = xgb_model.predict_proba(x_test)[:,1]
 print('XGB ROCAUC score:',roc_auc_score(y_test, xgb_predict))
 print('XGB Accuracy score:',accuracy_score(y_test, xgb_predict))
 print('XGB F1 score:',f1_score(y_test, xgb_predict))
 
 # build ensemble method
-comb_model = VotingClassifier(estimators=[('XBG',xgb_model), ('LR',lr), ('RF',rfc), ('SVC',svc_model)], voting='hard')
+comb_model = VotingClassifier(estimators=[('XBG',xgb_model), ('LR',lr), ('RF',rfc), ('SVC',svc_model)], voting='soft')
 comb_model.fit(x_train, y_train)
 comb_model_pred = comb_model.predict(x_test)
+comb_preds = comb_model.predict_proba(x_test)[:,1]
 
 print('Ensemble Model ROCAUC score:',roc_auc_score(y_test, comb_model_pred))
 print('Ensemble Model Accuracy score:',accuracy_score(y_test, comb_model_pred))
 print('Ensemble Model F1 score:',f1_score(y_test, comb_model_pred))
 
+fpr, tpr, _ = roc_curve(y_test, comb_preds)
+auc = round(roc_auc_score(y_test, comb_preds), 2)
+plt.plot(fpr, tpr, label="Non-Linear Model, AUC="+str(auc))
+plt.legend()
+plt.xlabel('1 - Specificity')
+plt.ylabel('Sensitivity')
+plt.show()
+
 # plot AUC
-fpr, tpr, _ = roc_curve(y_test, svc_predict)
-auc = round(roc_auc_score(y_test, svc_predict), 2)
-plt.plot(fpr,tpr,label="SVM, AUC="+str(auc))
-fpr, tpr, _ = roc_curve(y_test, lr_predict)
-auc = round(roc_auc_score(y_test, lr_predict), 2)
+
+fpr, tpr, _ = roc_curve(y_test, lr_preds)
+auc = round(roc_auc_score(y_test, lr_preds), 2)
 plt.plot(fpr, tpr, label="Multivariate Regression, AUC="+str(auc))
-fpr, tpr, _ = roc_curve(y_test, rfc_predict)
-auc = round(roc_auc_score(y_test, rfc_predict), 2)
+fpr, tpr, _ = roc_curve(y_test, rfc_preds)
+auc = round(roc_auc_score(y_test, rfc_preds), 2)
 plt.plot(fpr, tpr, label="Random Forest, AUC="+str(auc))
-fpr, tpr, _ = roc_curve(y_test, xgb_predict)
-auc = round(roc_auc_score(y_test, xgb_predict), 2)
+fpr, tpr, _ = roc_curve(y_test, xgb_preds)
+auc = round(roc_auc_score(y_test, xgb_preds), 2)
 plt.plot(fpr, tpr, label="XGBoost Classifier, AUC="+str(auc))
-fpr, tpr, _ = roc_curve(survival_yhat, pred_test_cl)
-auc = round(roc_auc_score(survival_yhat, pred_test_cl), 2)
+fpr, tpr, _ = roc_curve(y_test, svc_preds)
+auc = round(roc_auc_score(y_test, svc_preds), 2)
+plt.plot(fpr,tpr,label="SVM, AUC="+str(auc))
+fpr, tpr, _ = roc_curve(survival_yhat, preds[:,1])
+auc = round(roc_auc_score(survival_yhat, preds[:,1]), 2)
 plt.plot(fpr, tpr, label="Fully Connected Network, AUC="+str(auc))
-fpr, tpr, _ = roc_curve(y_test, comb_model_pred)
-auc = round(roc_auc_score(y_test, comb_model_pred), 2)
+fpr, tpr, _ = roc_curve(y_test, comb_preds)
+auc = round(roc_auc_score(y_test, comb_preds), 2)
 plt.plot(fpr, tpr, label="Ensemble Classifier, AUC="+str(auc))
 plt.legend()
 plt.xlabel('1 - Specificity')
 plt.ylabel('Sensitivity')
 plt.title('Survival Models Comparison')
 plt.show()
+
 
 # DeLong test
 
@@ -327,3 +356,4 @@ def delong_roc_test(ground_truth, predictions_one, predictions_two):
     return calc_pvalue(aucs, delongcov)
 
 print('DeLong test for non-linear and linear predictions:', delong_roc_test(y_test, svc_predict, lr_predict))
+
